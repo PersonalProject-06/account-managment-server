be29c912136558869783ffbba00e4103bede931b
firas-debich <debichf02@gmail.com>
2021-12-04 21:16:23 +0100

    deploy data base done

--- src/dataBase/dataBase.ts
+++ src/dataBase/dataBase.ts
@@ -2,16 +2,14 @@ import {createConnection} from "typeorm"
 import { Users} from "../Entites/User.entity"
 import dotenv from "dotenv"
 dotenv.config()
-const  {DATABASE, USERNAME, PASSWORD} =process.env
-console.log(process.env.DATABASE,process.env.HOST ,process.env.USERNAME,process.env.PASSWORD )
+
 export const Connection = createConnection({
-    type:"postgres",
-    database: process.env.DATABASE,
-    host:process.env.HOST,
-    username:process.env.USER,
-    password:process.env.PASSWORD,
-    logging:true,
-    
+   
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    extra: {
+        ssl: true
+      },
     synchronize:false,
     entities:[Users]
 })
