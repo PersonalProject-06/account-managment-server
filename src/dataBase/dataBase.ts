3305514b72f09f42d1fc1f666b09b56e253e7e8a
firas-debich <debichf02@gmail.com>
2021-12-04 21:19:20 +0100

    deploy data base done

--- src/dataBase/dataBase.ts
+++ src/dataBase/dataBase.ts
@@ -7,9 +7,7 @@ export const Connection = createConnection({
    
     type: 'postgres',
     url: process.env.DATABASE_URL,
-    extra: {
-        ssl: true
-      },
+   
     synchronize:false,
     entities:[Users]
 })
