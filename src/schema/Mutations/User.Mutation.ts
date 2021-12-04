8010ff548942ba296aa4d8fa16986c01ead48ae9
firas-debich <debichf02@gmail.com>
2021-12-04 21:40:11 +0100

    hash the password done

--- src/schema/Mutations/User.Mutation.ts
+++ src/schema/Mutations/User.Mutation.ts
@@ -1,7 +1,8 @@
 import {GraphQLString} from "graphql"
 import { UserType } from "../TypeDefs/User.typeDefs"
 import { Users} from "../../Entites/User.entity"
- import { User} from "../../Interface/User.interface"
+import { User} from "../../Interface/User.interface"
+import bcryptjs from "bcryptjs"
 export const CREATE_USER ={
 
     type : UserType,
@@ -11,8 +12,9 @@ export const CREATE_USER ={
         password : {type :GraphQLString}
     },
     resolve :async (parent:any , args:User):Promise<User> =>{
-        console.log(args ,'daz')
-        Users.insert(args)
+        const {name , email , password} = args
+        const passHashed = await bcryptjs.hash(password,10)
+       await Users.insert({name, email,password:passHashed })
         return args
      }
 
