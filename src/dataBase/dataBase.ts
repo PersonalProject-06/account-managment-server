07cd380ab5e0cedb77e46d778d74b6638edb15f8
firas-debich <debichf02@gmail.com>
2021-12-04 21:01:20 +0100

    deploy data base done

--- src/dataBase/dataBase.ts
+++ src/dataBase/dataBase.ts
@@ -3,12 +3,12 @@ import { Users} from "../Entites/User.entity"
 import dotenv from "dotenv"
 dotenv.config()
 const  {DATABASE, USERNAME, PASSWORD} =process.env
-
+console.log(process.env.DATABASE,process.env.HOST ,process.env.USERNAME,process.env.PASSWORD )
 export const Connection = createConnection({
     type:"postgres",
     database: process.env.DATABASE,
     host:process.env.HOST,
-    username:process.env.USERNAME,
+    username:process.env.USER,
     password:process.env.PASSWORD,
     logging:true,
     
