3ba6001dca6ac59f954569425477180b3fd44f0d
firas-debich <debichf02@gmail.com>
2021-12-04 20:42:23 +0100

    deploy data base done

--- src/dataBase/dataBase.ts
+++ src/dataBase/dataBase.ts
@@ -6,11 +6,12 @@ const  {DATABASE, USERNAME, PASSWORD} =process.env
 
 export const Connection = createConnection({
     type:"postgres",
-    database: DATABASE,
-    host:"localhost",
-    username: "postgres",
-    password:PASSWORD,
+    database: process.env.DATABASE,
+    host:process.env.HOST,
+    username:process.env.USERNAME,
+    password:process.env.PASSWORD,
     logging:true,
+    ssl:true,
     synchronize:false,
     entities:[Users]
 })
