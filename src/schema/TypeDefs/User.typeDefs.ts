7977340812a91256a2ef96f3eac3ac6d704261b4
firas-debich <debichf02@gmail.com>
2021-12-05 15:38:17 +0100

    issues with data base

--- src/schema/TypeDefs/User.typeDefs.ts
+++ src/schema/TypeDefs/User.typeDefs.ts
@@ -1,7 +1,7 @@
 import { GraphQLObjectType , GraphQLID , GraphQLString   } from 'graphql'
 
 export const UserType = new GraphQLObjectType({
-    name :"User" , 
+    name :"users" , 
     fields : ()=>({
         id : {type : GraphQLID},
         name : {type : GraphQLString},
